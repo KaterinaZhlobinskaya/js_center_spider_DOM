7da040331fcfc55b75e46f921bf2b6478b2dbbe4
Katya <evil.666.buny@gmail.com>
2024-07-24 11:44:50 +0300

    make build

---
+++ index.adc62e08.js
@@ -0,0 +1,2 @@
+var t,e,i,h;t=document.querySelector(".spider"),i={width:(e=document.querySelector(".wall")).clientWidth,height:e.clientHeight},h={width:t.clientWidth,height:t.clientHeight},t.style.position="relative",t.style.left="".concat(i.width/2-h.width/2,"px"),t.style.top="".concat(i.height/2-h.height/2,"px");
+//# sourceMappingURL=index.adc62e08.js.map
